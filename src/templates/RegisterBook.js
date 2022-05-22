f1ab0289151716c70af7cc87cb7f285f6ee73474
Jorge M. R. de Farias <jorge_ramos13@outlook.com.br>
2022-05-22 15:33:36 -0300

    Tabs OK

--- src/templates/RegisterBook.js
+++ src/templates/RegisterBook.js
@@ -1,14 +1,15 @@
+import { Container } from "@mui/system";
 import React, { useState } from "react";
 import { Button, Form, Input } from "semantic-ui-react";
 
 
-export const RegisterBook = ({onUpdate}) => {
+export const RegisterBook = ({ onUpdate }) => {
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
 
     const postData = async () => {
         if (title !== "" && author !== "") {
-            const book = {title, author}
+            const book = { title, author }
             const res = await fetch("/add_book", {
                 method: 'POST',
                 headers: {
@@ -23,23 +24,26 @@ export const RegisterBook = ({onUpdate}) => {
                 setAuthor("");
             }
         }
-         else {
+        else {
             console.log("Good lord, this thing is taking longer than expected.")
         }
     }
 
     return (
-        <Form>
-            <Form.Field>
-                <Input placeholder="Book title" value={title} onChange={e=> setTitle(e.target.value)}/>
-            </Form.Field>
-            <Form.Field>
-                <Input placeholder="Book author" value={author} onChange={e=> setAuthor(e.target.value)}/>
-            </Form.Field>
-            <Form.Field>
-                <Button onClick={postData}>Register</Button>
-            </Form.Field>
-        </Form>
+        <Container>
+            <Form>
+                <Form.Field>
+                    <Input placeholder="Book title" value={title} onChange={e => setTitle(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <Input placeholder="Book author" value={author} onChange={e => setAuthor(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <Button onClick={postData}>Register</Button>
+                </Form.Field>
+            </Form>
+        </Container>
+
     )
 
 }
